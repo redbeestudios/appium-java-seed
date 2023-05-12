5d2fa9eb22f8c0d87202a9d29f07640260531db9
cfontanaredbee <carlos.fontana@redb.ee>
2023-05-12 15:35:01 -0300

    add capabilities helper

--- src/test/java/io/redbee/appium/ExampleTest.java
+++ src/test/java/io/redbee/appium/ExampleTest.java
@@ -2,10 +2,12 @@ package io.redbee.appium;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import io.redbee.core.CapabilitiesHelper;
+
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
-import java.net.MalformedURLException;
+ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class ExampleTest{
@@ -13,13 +15,10 @@ public class ExampleTest{
     @Test
     public void first_test() throws MalformedURLException {
         AppiumDriver driver;
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("platformVersion", "11.0");
-        capabilities.setCapability("deviceName", "Pixel_3_API_30");
-        capabilities.setCapability("appWaitActivity", "*");
-        capabilities.setCapability("app",
-                System.getProperty("user.dir") + "/apps/Demo.apk");
+
+        CapabilitiesHelper helper = new CapabilitiesHelper();
+      //  DesiredCapabilities capabilities = helper.getCapabilities();
+        DesiredCapabilities capabilities = helper.getCapabilities("otherCapabilities.properties");
         driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
         driver.quit();
     }
