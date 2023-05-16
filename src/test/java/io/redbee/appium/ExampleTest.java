fc6edf7af231ca5e3e03a5d71fbba5abfc24d30c
Alejandro Lartigue <alejandro.lartigue@redb.ee>
2023-05-16 17:19:17 -0300

    Add AppiumServerHelper to Example Test

--- src/test/java/io/redbee/appium/ExampleTest.java
+++ src/test/java/io/redbee/appium/ExampleTest.java
@@ -2,9 +2,13 @@ package io.redbee.appium;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import io.redbee.core.AppiumServerHelper;
 import io.redbee.core.CapabilitiesHelper;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
  import java.net.MalformedURLException;
@@ -12,18 +16,22 @@ import java.net.URL;
 
 public class ExampleTest{
 
-    @Test
-    public void first_test() throws MalformedURLException {
+    AppiumServerHelper  server = new AppiumServerHelper();
+
+    @BeforeMethod(groups = {"regression"})
+    public void setUP(){
+        server.stop();
+        server.start();
+    }
+
+    @Test(groups = {"regression"})
+    public void first_test() {
         AppiumDriver driver;
 
         CapabilitiesHelper helper = new CapabilitiesHelper();
       //  DesiredCapabilities capabilities = helper.getCapabilities();
         DesiredCapabilities capabilities = helper.getCapabilities("otherCapabilities.properties");
-        driver = new AndroidDriver(new URL("http://0.0.0.0:4723/wd/hub"), capabilities);
+        driver = new AndroidDriver(server.getService(), capabilities);
         driver.quit();
     }
-
-
-
-
 }
